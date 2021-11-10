a44ed2095c693c6089b59232ecf8258c90116d5b
Esad Dursun <3723esad@gmail.com>
2021-11-10 20:07:51 +0100

    initial commit. home and details page basically finished. complex ui design implemented

---
+++ lib/colors.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color homePageBackground = const Color(0xFFF1F8E9);
+  static Color gradientFirst = const Color(0xFF33691E);
+  static Color gradientSecond = const Color(0xFFC5E1A5);
+  static Color homePageTitle = const Color(0xFF004D40);
+  static Color homePageSubtitle = const Color(0xFF00796B);
+  static Color homePageDetail = const Color(0xFF00BFA5);
+  static Color homePageIcons = const Color(0xFF1B5E20);
+  static Color homePageContainerTextSmall = const Color(0xFFF6FDF4);
+  static Color homePageContainerTextBig = const Color(0xFFFFFFFF);
+  static Color homePagePlanColor = const Color(0xFF4DB6AC);
+  static Color secondPageTopIconColor = const Color(0xFFB7E8BD);
+  static Color secondPageTitleColor = const Color(0xFFFEFFFE);
+  static Color secondPageContainerGradient1stColor = const Color(0xFF66BB6A);
+  static Color secondPageContainerGradient2ndColor = const Color(0xFF81C784);
+  static Color secondPageIconColor = const Color(0xFFFAFEFA);
+  static Color loopColor = const Color(0xFFA7FFEB);
+  static Color setsColor = const Color(0xFFB7C2B8);
+  static Color circuitsColor = const Color(0xFF2B5914);
+}
