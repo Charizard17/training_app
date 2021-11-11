f67103df1112d258181184bfde6daae15cba3c75
Esad Dursun <3723esad@gmail.com>
2021-11-11 21:01:50 +0100

    video info page created. lot of improvements. working on ui

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
 
 import './home_page.dart';
 
@@ -16,12 +17,16 @@ class VideoInfo extends StatefulWidget {
 }
 
 class _VideoInfoState extends State<VideoInfo> {
-  List info = [];
-  _initData() {
-    DefaultAssetBundle.of(context)
+  List videoInfo = [];
+  bool _playArea = false;
+  late VideoPlayerController _contoller;
+  _initData() async {
+    await DefaultAssetBundle.of(context)
         .loadString("json/videoinfo.json")
         .then((value) {
-      info = json.decode(value);
+      setState(() {
+        videoInfo = json.decode(value);
+      });
     });
   }
 
@@ -35,142 +40,184 @@ class _VideoInfoState extends State<VideoInfo> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.AppColor.gradientFirst.withOpacity(0.9),
-              color.AppColor.gradientSecond,
-            ],
-            begin: const FractionalOffset(0.0, 0.4),
-            end: Alignment.topRight,
-          ),
-        ),
+        decoration: _playArea == false
+            ? BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    color.AppColor.gradientFirst.withOpacity(0.9),
+                    color.AppColor.gradientSecond,
+                  ],
+                  begin: const FractionalOffset(0.0, 0.4),
+                  end: Alignment.topRight,
+                ),
+              )
+            : BoxDecoration(
+                color: color.AppColor.gradientSecond,
+              ),
         child: Column(
           children: [
-            Container(
-              padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
-              width: MediaQuery.of(context).size.width,
-              height: 300,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Get.to(() => HomePage());
-                        },
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          size: 20,
-                          color: color.AppColor.secondPageIconColor,
-                        ),
-                      ),
-                      Expanded(child: Container()),
-                      Icon(
-                        Icons.info_outline,
-                        size: 20,
-                        color: color.AppColor.secondPageIconColor,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Text(
-                    "Legs Toning",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: color.AppColor.secondPageTitleColor,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "and Glutes Workout",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: color.AppColor.secondPageTitleColor,
-                    ),
-                  ),
-                  SizedBox(height: 50),
-                  Row(
-                    children: [
-                      Container(
-                        width: 90,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: LinearGradient(
-                            colors: [
-                              color
-                                  .AppColor.secondPageContainerGradient1stColor,
-                              color.AppColor.secondPageContainerGradient2ndColor
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+            _playArea == false
+                ? Container(
+                    padding:
+                        const EdgeInsets.only(top: 60, left: 20, right: 20),
+                    width: MediaQuery.of(context).size.width,
+                    height: 250,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
                           children: [
+                            InkWell(
+                              onTap: () {
+                                Get.to(() => HomePage());
+                              },
+                              child: Icon(
+                                Icons.arrow_back_ios,
+                                size: 20,
+                                color: color.AppColor.secondPageIconColor,
+                              ),
+                            ),
+                            Expanded(child: Container()),
                             Icon(
-                              Icons.timer,
+                              Icons.info_outline,
                               size: 20,
                               color: color.AppColor.secondPageIconColor,
                             ),
-                            SizedBox(width: 5),
-                            Text(
-                              "68 min",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: color.AppColor.secondPageIconColor,
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          "Legs Toning",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: color.AppColor.secondPageTitleColor,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "and Glutes Workout",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: color.AppColor.secondPageTitleColor,
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Row(
+                          children: [
+                            Container(
+                              width: 90,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                  colors: [
+                                    color.AppColor
+                                        .secondPageContainerGradient1stColor,
+                                    color.AppColor
+                                        .secondPageContainerGradient2ndColor
+                                  ],
+                                  begin: Alignment.bottomLeft,
+                                  end: Alignment.topRight,
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.timer,
+                                    size: 20,
+                                    color: color.AppColor.secondPageIconColor,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    "68 min",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: color.AppColor.secondPageIconColor,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            Container(
+                              width: 220,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                  colors: [
+                                    color.AppColor
+                                        .secondPageContainerGradient1stColor,
+                                    color.AppColor
+                                        .secondPageContainerGradient2ndColor
+                                  ],
+                                  begin: Alignment.bottomLeft,
+                                  end: Alignment.topRight,
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.handyman_outlined,
+                                    size: 20,
+                                    color: color.AppColor.secondPageIconColor,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    "Resistent band, kettebell",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: color.AppColor.secondPageIconColor,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ],
                         ),
-                      ),
-                      SizedBox(width: 20),
-                      Container(
-                        width: 220,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          gradient: LinearGradient(
-                            colors: [
-                              color
-                                  .AppColor.secondPageContainerGradient1stColor,
-                              color.AppColor.secondPageContainerGradient2ndColor
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topRight,
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.handyman_outlined,
-                              size: 20,
-                              color: color.AppColor.secondPageIconColor,
-                            ),
-                            SizedBox(width: 5),
-                            Text(
-                              "Resistent band, kettebell",
-                              style: TextStyle(
-                                fontSize: 14,
+                      ],
+                    ),
+                  )
+                : Container(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 250,
+                          padding: const EdgeInsets.only(
+                              top: 60, left: 20, right: 20),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  Get.to(() => HomePage());
+                                },
+                                child: Icon(
+                                  Icons.arrow_back_ios,
+                                  size: 20,
+                                  color: color.AppColor.secondPageIconColor,
+                                ),
+                              ),
+                              Expanded(child: Container()),
+                              Icon(
+                                Icons.info_outline,
+                                size: 20,
                                 color: color.AppColor.secondPageIconColor,
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                        _playView(context),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: color.AppColor.homePageBackground,
                   borderRadius: BorderRadius.only(
                     topRight: Radius.circular(70),
                   ),
@@ -197,7 +244,7 @@ class _VideoInfoState extends State<VideoInfo> {
                               size: 30,
                               color: color.AppColor.loopColor,
                             ),
-                            SizedBox(width: 10),
+                            SizedBox(width: 5),
                             Text(
                               "3 sets",
                               style: TextStyle(
@@ -209,13 +256,149 @@ class _VideoInfoState extends State<VideoInfo> {
                         ),
                         SizedBox(width: 20),
                       ],
-                    )
+                    ),
+                    Expanded(
+                      child: _listView(),
+                    ),
                   ],
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _playView(BuildContext context) {
+    final controller = _contoller;
+    if (controller != null && controller.value.isInitialized) {
+      return AspectRatio(
+        aspectRatio: 16 / 9,
+        child: VideoPlayer(controller),
+      );
+    } else {
+      return Text('Being initialized, please wait..');
+    }
+  }
+
+  _onTapVideo(int index) {
+    final controller =
+        VideoPlayerController.network(videoInfo[index]['videoUrl']);
+    _contoller = controller;
+    setState(() {});
+    // controller
+    //   ..initialize().then((_) {
+    //     setState(() {
+    //       controller.play();
+    //     });
+    //   });
+  }
+
+  _listView() {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      itemCount: videoInfo.length,
+      itemBuilder: (_, int index) {
+        return GestureDetector(
+          onTap: () {
+            _onTapVideo(index);
+            debugPrint(index.toString());
+            setState(() {
+              if (_playArea == false) {
+                _playArea = true;
+              }
+            });
+          },
+          child: _buildCard(index),
+        );
+      },
+    );
+  }
+
+  _buildCard(int index) {
+    return Container(
+      height: 120,
+      margin: EdgeInsets.only(bottom: 5),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: AssetImage(
+                        videoInfo[index]['thumbnail'],
+                      ),
+                      fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(width: 10),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    videoInfo[index]['title'],
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: color.AppColor.circuitsColor),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: Text(
+                      videoInfo[index]['time'],
+                      style: TextStyle(color: color.AppColor.setsColor),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Row(
+            children: [
+              Container(
+                width: 80,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: Color(0xFFEAEEFC),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    '15s rest',
+                    style: TextStyle(
+                      color: Color(0xFF839FED),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  for (int i = 0; i < 70; ++i)
+                    i.isOdd
+                        ? Container(
+                            width: 3,
+                            height: 1,
+                            decoration: BoxDecoration(
+                                color: Color(0xFF839FED),
+                                borderRadius: BorderRadius.circular(2)))
+                        : Container(
+                            width: 3,
+                            height: 1,
+                            color: color.AppColor.homePageBackground,
+                          )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
